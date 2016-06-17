21986870cefbc05bcbf6139e43cc037d73daaaee
Alex Nelson <ascotnelson@gmail.com>
2016-06-17 19:56:19 +0000

    add mario.c with passing test

--- mario.c
+++ mario.c
@@ -1,30 +1,42 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int main()
+int main(void)
 {
-    int lines, height, spaces;
+    // declare and intialize height
+    int height;
+    height = 0;
     
-    
-    /* prompt user for height */
     {
-    do
-        printf ("Hey Grand Architect! How high of a pyramid would please you? ");
-        height = getInt ();
-    }
-    while (height < 1 || height > 23) /* prompt again if invalid entry */
+    printf ("\n\nHey Grand Architect!\n\n");
     
-    for (lines = 0, lines < height, lines++);
-    {
-        //print spaces
-        spaces = height - 1;
-        spaces--;
-        printf(" ");
-        
-        //print hashes
-        //print new lines
+    printf ("I shall build a glorious pyramid in your honor.\n\n");
     }
-    //code outside
-        
- 
+
+    // prompt for user input
+    do 
+    {
+	printf ("How high of a pyramid would please you? ");
+	height = GetInt();
+	
+	}
+	//accepts only input in this range
+    while (height < 0 || height > 23);
+
+    
+    for (int lines = 0; lines < height; lines++) 
+    {
+	// print spaces
+	for (int spaces = 0; spaces < height - lines - 1; spaces++) 
+	    {
+	    printf(" ");
+	    }
+	// print hashes
+	for (int hashes = 0; hashes < lines + 2; hashes++) 
+	    {
+	    printf("#");
+	    }
+	printf("\n");
+    }
+    return 0;
 }
